c173c17564aeb4627dd9d69b95fb46f6fed6afda
Zohaib <m.zohaibhanif@gmail.com>
2021-03-29 19:25:43 +0000

    adding scripts

---
+++ input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''MZ || m.zohaibhanif@gmail.com
+Best practice script and learning about imput function'''
+
+def main():
+    uriAnswer = input("What is the URL you would like looked up:")
+
+    print(uriAnswer)
+
+main()
